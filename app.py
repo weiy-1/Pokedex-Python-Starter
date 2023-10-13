4070b515371da9eb47d2be562336c39fec309499
mr. whalen <michael.whalen@sitechhs.com>
2023-10-13 13:25:57 -0400

    dfgh

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 ## Open the JSON file of pokemon data
-pokedex = open("./pokemon/pokedex.json", encoding="utf8")
+pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
+print(data[0])
